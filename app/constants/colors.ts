7eb7c950b103d3cbc4b3835d31e961f39dc9d1ce
Carlos Quesada <carlosquesadaumana@gmail.com>
2024-08-07 14:07:43 -0600

    Initial constants added

---
+++ app/constants/colors.ts
@@ -0,0 +1,23 @@
+export const THEMES = {
+    LIGHT: {
+        BACKGROUND_COLOR: "#F5F6F8",
+        TEXT_COLOR: "#292929",
+        ITEMS_BG_COLOR: "#FFFFFF",
+    },
+    DARK: {
+        BACKGROUND_COLOR: "#1F1F21",
+        TEXT_COLOR: "#F5F6F8",
+        ITEMS_BG_COLOR: "#292929",
+    },
+}
+
+
+export const COLORS = {
+    PRIMARY: "#D0FF41",
+    SECONDARY: "#FF7800",
+    INFO: "#4E6BD1",
+    DANGER: "#B03232",
+    SUCCESS: "#A3BF7F"
+}
+
+export const PADDING = "16px"
---
+++ app/constants/index.ts
@@ -0,0 +1,18 @@
+
+export const ADD_PET_FORM_STEPS = {
+    1: "BASIC_INFORMATION",
+    2: "PET_CHARACTERISTICS",
+    3: "PET_MEDICAL_HISTORY",
+}
+
+export const PET_MEDICAL_HISTORY_TYPES = {
+    1: "VACCINES",
+    2: "DIAGNOSTICS",
+    3: "VACCINES",
+    4: "ALLERGIES",
+}
+
+export const AUTH_OPTIONS = {
+    1: "STANDARD",
+    2: "WITH GOOGLE"
+}
