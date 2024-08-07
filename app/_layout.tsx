39004b8e2886c4621b9b7a03a682d62bd0d67df5
Carlos Quesada <carlosquesadaumana@gmail.com>
2024-08-06 20:14:11 -0600

    Navbar deleted

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,20 +1,12 @@
 import React from "react";
-import { Link, Slot } from "expo-router";
+import { Slot } from "expo-router";
 import { StyleSheet } from "react-native";
-import { View, Text } from "react-native";
+import { View } from "react-native";
 
 const Layout = () => {
   return (
     <View style={styles.container}>
       <Slot />
-
-      <Link href="/" style={styles.link}>
-        Ir a Mis mascotas
-      </Link>
-
-      <Link href="/profile" style={styles.link}>
-        Ir a Perfil
-      </Link>
     </View>
   );
 };
