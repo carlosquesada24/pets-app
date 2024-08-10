d305bfa2d364490752063e00595d06b47e25e28b
Carlos Quesada <carlosquesadaumana@gmail.com>
2024-08-10 10:02:56 -0600

    AuthContext created

---
+++ app/(modules)/authentication/domain/data.ts
@@ -0,0 +1,11 @@
+export const USER_DEFAULT_STATE = {
+    id: "",
+    email: "",
+    password: "",
+}
+
+export const USER_MOCK = {
+    id: "1",
+    email: "firstexample@example.com",
+    password: "123456789",
+}
