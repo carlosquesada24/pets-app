2e4aea3dbabb249ee5eeeff7b0540b2c6a22d5d5
Carlos Quesada <carlosquesadaumana@gmail.com>
2024-08-10 10:19:51 -0600

    Domain consts and interfaces of pets exported to my-pets module

---
+++ app/(modules)/my-pets/domain/data.ts
@@ -0,0 +1,167 @@
+export const PET_DEFAULT_STATE = {
+    id: "pet1",
+    name: "Pet 1",
+    details: {
+        information: {
+            height: "120 cm",
+            weight: "12 kg",
+            age: "5 years old",
+            breed: "American Stafford"
+        },
+        medical: {
+            diagnoses: [
+                {
+                    date: "01/01/2024",
+                    diagnosis: "Diagnosis 1"
+                },
+                {
+                    date: "02/01/2024",
+                    diagnosis: "Diagnosis 2"
+                }
+            ],
+            allergies: [
+                {
+                    allergy: "Pollen Allergy"
+                }
+            ],
+            medicines: [
+                {
+                    medicine: "Medicine A",
+                    dosage: "10 mg",
+                    frequency: "Once a day"
+                }
+            ],
+            vaccines: [
+                {
+                    vaccine: "Rabies",
+                    date: "15/06/2023"
+                }
+            ]
+        }
+    }
+};
+
+export const PET_LIST_DEFAULT_STATE = [{
+    id: "pet1",
+    name: "Pet 1",
+    details: {
+        information: {
+            height: "120 cm",
+            weight: "12 kg",
+            age: "5 years old",
+            breed: "American Stafford"
+        },
+        medical: {
+            diagnoses: [
+                {
+                    date: "01/01/2024",
+                    diagnosis: "Diagnosis 1"
+                },
+                {
+                    date: "02/01/2024",
+                    diagnosis: "Diagnosis 2"
+                }
+            ],
+            allergies: [
+                {
+                    allergy: "Pollen Allergy"
+                }
+            ],
+            medicines: [
+                {
+                    medicine: "Medicine A",
+                    dosage: "10 mg",
+                    frequency: "Once a day"
+                }
+            ],
+            vaccines: [
+                {
+                    vaccine: "Rabies",
+                    date: "15/06/2023"
+                }
+            ]
+        }
+    }
+}, {
+    id: "pet2",
+    name: "Pet 2",
+    details: {
+        information: {
+            height: "120 cm",
+            weight: "12 kg",
+            age: "5 years old",
+            breed: "American Stafford"
+        },
+        medical: {
+            diagnoses: [
+                {
+                    date: "01/01/2024",
+                    diagnosis: "Diagnosis 1"
+                },
+                {
+                    date: "02/01/2024",
+                    diagnosis: "Diagnosis 2"
+                }
+            ],
+            allergies: [
+                {
+                    allergy: "Pollen Allergy"
+                }
+            ],
+            medicines: [
+                {
+                    medicine: "Medicine A",
+                    dosage: "10 mg",
+                    frequency: "Once a day"
+                }
+            ],
+            vaccines: [
+                {
+                    vaccine: "Rabies",
+                    date: "15/06/2023"
+                }
+            ]
+        }
+    }
+}, {
+    id: "pet3",
+    name: "Pet 3",
+    details: {
+        information: {
+            height: "120 cm",
+            weight: "12 kg",
+            age: "5 years old",
+            breed: "American Stafford"
+        },
+        medical: {
+            diagnoses: [
+                {
+                    date: "01/01/2024",
+                    diagnosis: "Diagnosis 1"
+                },
+                {
+                    date: "02/01/2024",
+                    diagnosis: "Diagnosis 2"
+                }
+            ],
+            allergies: [
+                {
+                    allergy: "Pollen Allergy"
+                }
+            ],
+            medicines: [
+                {
+                    medicine: "Medicine A",
+                    dosage: "10 mg",
+                    frequency: "Once a day"
+                }
+            ],
+            vaccines: [
+                {
+                    vaccine: "Rabies",
+                    date: "15/06/2023"
+                }
+            ]
+        }
+    }
+}]
