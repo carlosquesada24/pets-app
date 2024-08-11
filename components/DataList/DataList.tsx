16f651181dd92092e23cd167f4ca80bed07c7726
Carlos Quesada <carlosquesadaumana@gmail.com>
2024-08-11 08:39:40 -0600

    DataList structure more dynamicly

--- components/DataList/DataList.tsx
+++ components/DataList/DataList.tsx
@@ -11,27 +11,26 @@ const DataList = ({ dataList, title }: DataListProps) => {
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <Text style={styles.text}>{title}</Text>
-        <CustomButton type="primary" text="+" onPress={() => {}} />
+        <Text style={{ ...styles.text, ...styles.title }}>{title}</Text>
+        <CustomButton
+          customStyles={styles.customButton}
+          type="primary"
+          text="+"
+          onPress={() => {}}
+        />
       </View>
       <View style={styles.list}>
-        <View>
-          <Text>24-07-2024</Text>
-          <View>
-            <Text>Title of the item</Text>
-            <Text>Lorem ipsum lorem ipsum lorem ipsum lore...</Text>
-          </View>
-        </View>
+        {dataList.map((item, index) => {
+          return (
+            <View key={index} style={styles.listItem}>
+              <Text style={styles.text}>{item.date}</Text>
+              <View style={styles.listItemContent}>
+                <Text style={styles.text}>{item.name ?? item.text}</Text>
+              </View>
+            </View>
+          );
+        })}
       </View>
-      {/* <View>
-        <button className="pagination-button">&lt;</button>
-        <button className="pagination-button active">1</button>
-        <button className="pagination-button">2</button>
-        <button className="pagination-button">3</button>
-        <span>...</span>
-        <button className="pagination-button">8</button>
-        <button className="pagination-button">&gt;</button>
-      </View> */}
     </View>
   );
 };
@@ -40,25 +39,40 @@ const styles = StyleSheet.create({
   text: {
     color: "#fff",
   },
-  title: {
-    fontSize: 30,
-    fontWeight: "bold",
-    marginBottom: 10,
-  },
   header: {
     width: "100%",
     display: "flex",
     justifyContent: "space-between",
     alignItems: "center",
+    flexDirection: "row",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  customButton: {
+    padding: 6,
+    fontWeight: "bold",
   },
   list: {
+    marginTop: 16,
     display: "flex",
     justifyContent: "space-between",
     alignItems: "center",
     flexDirection: "column",
   },
+  listItem: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    flexDirection: "row",
+  },
+  listItemContent: {
+    marginLeft: 12,
+  },
   container: {
     width: "100%",
+    padding: 12,
   },
 });
 
