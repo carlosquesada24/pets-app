e61aeca8ad86f2258503753d4686e563dbb00ac9
Carlos Quesada <carlosquesadaumana@gmail.com>
2024-08-19 16:13:38 -0600

    Logic decoupled from DataList and data visualization error fixed

--- components/DataList/DataList.tsx
+++ components/DataList/DataList.tsx
@@ -1,66 +1,41 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import CustomButton from "../Button";
-import { randomUUID } from "expo-crypto";
-import { formatTextPreview } from "../../app/utils/string";
 import ListItem from "./components/ListItem/ListItem";
 
 interface DataListProps {
   dataList: any[];
   title: string;
   handleAddItem: any;
+  handleEditItem?: any;
 }
 
 interface ListItem {
   id: string;
   date: string;
   name: string;
-  isEditing: boolean;
+  isEditing?: boolean;
+  isCreating?: boolean;
 }
 
-const DataList = ({ dataList, title, handleAddItem }: DataListProps) => {
+const DataList = ({
+  dataList,
+  title,
+  handleAddItem,
+  handleEditItem,
+}: DataListProps) => {
   const [list, setList] = useState<any[]>(dataList);
-  const [isCreating, setIsCreating] = useState(false);
-  const [isEditing, setIsEditing] = useState(false);
+
+  useEffect(() => {
+    setList(dataList);
+  }, [dataList]);
 
   const handleAddItemPress = () => {
-    // setList([
-    //   ...list,
-    //   {
-    //     date: new Date().toLocaleDateString(),
-    //     name: "New item",
-    //   },
-    // ]);
-    setIsCreating(true);
-
-    // Crear nuevo elemento en el list
-    setList([
-      ...list,
-      { id: randomUUID(), name: "", date: "17/08/2024", isCreating: true },
-    ]);
-  };
-
-  const handleEditItem = (item: any) => {
-    setIsEditing(true);
+    handleAddItem();
   };
 
   const handleOnAcceptCreation = (id: any, text: string) => {
-    setIsCreating(false);
-    setIsEditing(false);
-
-    const foundItem = list.find((item) => item.id === id);
-
-    const updatedItem = {
-      ...foundItem,
-      name: text,
-      isCreating: false,
-    };
-
-    const updatedList = list.map((item) =>
-      item.id === updatedItem.id ? updatedItem : item
-    );
-
-    setList(updatedList);
+    handleEditItem(id, text);
   };
 
   return (
@@ -84,8 +59,6 @@ const DataList = ({ dataList, title, handleAddItem }: DataListProps) => {
               />
             );
           })
-        ) : isCreating ? (
-          <Text style={styles.text}>Creando uno nuevo...z</Text>
         ) : (
           <Text style={styles.text}>No hay items aún</Text>
         )}
