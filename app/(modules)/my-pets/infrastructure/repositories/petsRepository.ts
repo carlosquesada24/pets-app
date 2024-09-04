a676102992e974cf35029f77186b9887422133fd
Carlos Quesada <carlosquesadaumana@gmail.com>
2024-09-03 20:41:46 -0600

    petsRepository created

---
+++ app/(modules)/my-pets/infrastructure/repositories/petsRepository.ts
@@ -0,0 +1,53 @@
+import { SQLiteDatabase } from "expo-sqlite";
+import { Pet } from "../../domain/interface";
+
+
+export const getAllPetsFromSQLite = async (db: SQLiteDatabase) => {
+    const result = await db.getAllAsync("SELECT * FROM Pets");
+
+    return result;
+}
+
+export const getPetByIdFromSQLite = async (id: string, db: SQLiteDatabase) => {
+    const result = await db.getFirstAsync("SELECT * FROM Pets WHERE id = ?", [id]);
+
+    return result;
+};
+
+export const savePetIntoSQLite = async (pet: Pet, db: SQLiteDatabase) => {
+    const result = await db.runAsync(
+        `INSERT INTO Pets (Pets.name, Pets.weight, Pets.height, Pets.breed, Pets.age, Pets.photoURL, Pets.isActive, Pets.createdAt, Pets.updatedAt)
+        VALUES (?, ?, ?, ?, ?, ?, ?, datetime('now'), datetime('now'))`,
+        [
+            pet.name,
+            pet.details.information.weight,
+            pet.details.information.height,
+            pet.details.information.breed,
+            pet.details.information.age,
+            pet.photoURL,
+            1,
+        ]
+    );
+
+    console.log({ petAdded: result, id: result.lastInsertRowId });
+
+    return result.lastInsertRowId;
+};
+
+export const updatePetIntoSQLite = async (updatedPet: Pet, id: string, db: SQLiteDatabase) => {
+    await db.runAsync(
+        `UPDATE Pets 
+           SET name = ?, weight = ?, height = ?, breed = ?, age = ?, photoURL = ?, isActive = ?, updatedAt = datetime('now')
+           WHERE id = ?`,
+        [
+            updatedPet.name,
+            updatedPet.details.information.weight,
+            updatedPet.details.information.height,
+            updatedPet.details.information.breed,
+            updatedPet.details.information.age,
+            updatedPet.photoURL,
+            1,
+            id,
+        ]
+    );
+};
