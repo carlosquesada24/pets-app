c3df70527f23057ded7f72e0111dba80455e0339
Carlos Quesada <carlos.quesada.webdev@gmail.com>
2024-09-12 13:30:52 -0600

    SignIn page created

---
+++ app/(modules)/authentication/(infrastructure)/(pages)/sign-in/index.tsx
@@ -0,0 +1,106 @@
+import React from "react";
+import { StyleSheet, Text, TextInput, View } from "react-native";
+import { useForm } from "../../../../../(hooks)/useForm";
+import CustomButton from "../../../../../../components/Button";
+
+import { signUpValidations } from "../../../(domain)/validations/sign-up";
+import { useAuth } from "../../(contexts)/AuthContext";
+import { useRouter } from "expo-router";
+
+const SIGN_IN_FORM_DEFAULT_STATE = {
+  email: "",
+  password: "",
+};
+
+const SignInPage = () => {
+  const {
+    values: formValues,
+    errors,
+    handleInputChange,
+  } = useForm(SIGN_IN_FORM_DEFAULT_STATE, signUpValidations);
+
+  const { logIn } = useAuth();
+
+  const router = useRouter();
+
+  const handleSignIn = () => {
+    logIn(formValues.email, formValues.password);
+
+    router.push("/my-pets");
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Iniciar sesión</Text>
+
+      <View>
+        <TextInput
+          style={styles.input}
+          placeholder="admin@example.com"
+          placeholderTextColor="#a3a2a2"
+          onChangeText={(text) => handleInputChange("email", text)}
+        />
+
+        {errors.email &&
+          errors.email.map((error: string, index: number) => (
+            <Text key={index} style={styles.errorText}>
+              {error}
+            </Text>
+          ))}
+      </View>
+
+      <View>
+        <TextInput
+          style={styles.input}
+          secureTextEntry={true}
+          placeholder="ASKDNNDLKASND2313123"
+          placeholderTextColor="#a3a2a2"
+          onChangeText={(text) => handleInputChange("password", text)}
+        />
+
+        {errors.password &&
+          errors.password.map((error: string, index: number) => (
+            <Text key={index} style={styles.errorText}>
+              {error}
+            </Text>
+          ))}
+      </View>
+
+      <CustomButton
+        customStyles={styles.signInButton}
+        text="Iniciar sesión"
+        type="primary"
+        onPress={() => handleSignIn()}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "flex-start",
+    width: "100%",
+  },
+  text: {
+    color: "#fff",
+  },
+  errorText: {
+    color: "#f00",
+  },
+  input: {
+    padding: 8,
+    borderRadius: 8,
+    backgroundColor: "transparent",
+    color: "#fff",
+    marginTop: 4,
+    borderWidth: 2,
+    borderColor: "#242424",
+  },
+  signInButton: {
+    fontWeight: "bold",
+  },
+});
+
+export default SignInPage;
