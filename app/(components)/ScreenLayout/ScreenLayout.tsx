0e622f83561df8d39eab4b7d9bfe6424691352dd
Carlos Quesada <carlos.quesada.webdev@gmail.com>
2024-09-16 15:06:43 -0600

    ScreenLayout css styles fixed

--- app/(components)/ScreenLayout/ScreenLayout.tsx
+++ app/(components)/ScreenLayout/ScreenLayout.tsx
@@ -1,12 +1,20 @@
 import React from "react";
-import { ScrollView, View } from "react-native";
+import { ScrollView, StyleSheet, View } from "react-native";
+import { THEMES } from "../../constants/styles";
 
 const ScreenLayout = ({ children }: { children: React.JSX.Element }) => {
-  return (
-    <ScrollView style={{ backgroundColor: "#000", height: "100%" }}>
-      {children}
-    </ScrollView>
-  );
+  return <ScrollView style={styles.container}>{children}</ScrollView>;
 };
+
+const styles = StyleSheet.create({
+  container: {
+    height: "100%",
+    paddingTop: 12,
+    paddingLeft: 16,
+    paddingRight: 16,
+
+    backgroundColor: THEMES.DARK.BACKGROUND_COLOR,
+  },
+});
 
 export default ScreenLayout;
