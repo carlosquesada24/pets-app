0d9c983dc027f34b6918e903f804c35651eece2c
Carlos Quesada <carlos.quesada.webdev@gmail.com>
2024-09-17 12:26:03 -0600

    globalStyles created

---
+++ app/(styles)/global.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+export const globalStyles = StyleSheet.create({
+  text: {
+    fontFamily: "Inter",
+    color: "#fff",
+    fontSize: 18,
+  },
+  link: {
+    color: "#fff",
+    fontFamily: "Inter",
+  },
+});
