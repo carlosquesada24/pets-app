2d1f0a6b2d2c3ca36526dca22c5da0f56f19798d
Carlos Quesada <carlos.quesada.webdev@gmail.com>
2024-09-18 13:39:16 -0600

    Inter font family deleted from global styles

--- app/(styles)/global.tsx
+++ app/(styles)/global.tsx
@@ -2,12 +2,10 @@ import { StyleSheet } from "react-native";
 
 export const globalStyles = StyleSheet.create({
   text: {
-    fontFamily: "Inter",
     color: "#fff",
     fontSize: 18,
   },
   link: {
     color: "#fff",
-    fontFamily: "Inter",
   },
 });
