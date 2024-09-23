2fa7e390975819989d17d2f557f3bc9e732d8851
Carlos Quesada <carlos.quesada.webdev@gmail.com>
2024-09-23 14:08:38 -0600

    Delete all tables when initialize db added

--- app/(config)/(db)/index.ts
+++ app/(config)/(db)/index.ts
@@ -1,8 +1,18 @@
-import { INITIALIZE_DB } from "./queries/pets";
+import { SQLiteDatabase } from "expo-sqlite";
+import { DELETE_ALL_TABLES, INITIALIZE_DB } from "./queries/pets";
 
 
-export const initializeDatabase = async (db: any) => {
+export const initializeDatabase = async (db: SQLiteDatabase) => {
     try {
+
+        db.execAsync(DELETE_ALL_TABLES)
+            .then((result: any) => {
+                console.log({ result: result });
+            })
+            .catch((err: any) => {
+                console.log({ err });
+            });
+
         await db.execAsync(INITIALIZE_DB);
 
         console.log("DB INITIALIZED!")
